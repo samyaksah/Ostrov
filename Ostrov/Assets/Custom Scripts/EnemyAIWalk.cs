0b5fbe4f10de6594e6b3bbb47fd0e1eb4af0780f
samyaksah <samyakbbps@gmail.com>
2021-03-18 08:48:08 -0700

    Initial Work

---
+++ Ostrov/Assets/Custom Scripts/EnemyAIWalk.cs
@@ -0,0 +1,56 @@
+
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAIWalk : MonoBehaviour
+{
+    public NavMeshAgent agent;
+    public Transform player;
+    public LayerMask whatIsGround, whatIsPlayer;
+
+    public Vector3 walkPoint;
+    bool walkPointSet;
+    public float walkPointRange;
+
+    private void Awake()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+    private void Update()
+    {
+        Patrol();
+    }
+
+    private void Patrol()
+    {
+        if (!walkPointSet)
+        {
+            SearchWalkPoint();
+        }
+
+        if (walkPointSet)
+        {
+            agent.SetDestination(walkPoint);
+        }
+
+        Vector3 distanceToWalkPoint = transform.position - walkPoint;
+
+        if(distanceToWalkPoint.magnitude < 1f)
+        {
+            walkPointSet = false;
+        }
+    }
+
+    private void SearchWalkPoint()
+    {
+        float randomZ = Random.Range(-walkPointRange, walkPointRange);
+        float randomX = Random.Range(-walkPointRange, walkPointRange);
+
+        walkPoint = new Vector3(transform.position.x + randomX, transform.position.y, transform.position.z + randomZ);
+
+        if(Physics.Raycast(walkPoint, -transform.up, 2f, whatIsGround))
+        {
+            walkPointSet = true;
+        }
+    }
+}
